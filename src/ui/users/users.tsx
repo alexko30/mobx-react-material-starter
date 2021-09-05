f9ef9eff40bbdc57975dda8e368b0ae1af27e9b3
Oleksandr Korniienko <korniienko.alexander@gmail.com>
2021-09-06 00:04:42 +0300

    precommit setup

--- src/ui/users/users.tsx
+++ src/ui/users/users.tsx
@@ -16,14 +16,6 @@ const Users: React.FC<UsersProps> = (props) => {
       { component: UserList, path: ROUTES.private.users.root, key: ROUTES.private.users.root },
       { component: UserManagement, path: ROUTES.private.users.children.management(), key: ROUTES.private.users.children.management() },
     ];
-
-
-
-
-
-
-
-    
   }, []);
 
   return (
